b754ae33e4d99b558d92bdea514c25243961da67
Tanja Rietveldt <diafox2015@gmail.com>
2022-01-31 14:32:12 +0100

    create custom Provider class

--- boot/unitfun/tests/categ_factory.py
+++ boot/unitfun/tests/categ_factory.py
@@ -1,20 +1,32 @@
 import factory
+import faker.providers
 from factory.fuzzy import FuzzyDecimal, FuzzyChoice, FuzzyText
 from faker import Faker
 from unitfun.models import Category, Product
+from .categ_list import CATEGORIES
+from .prod_list import PRODUCTS
+
+
+class Provider(faker.providers.BaseProvider):
+    """ create custom class with custom method based on self.random_element(...)"""
+
+    def ecomm_categs(self):
+        return self.random_element(CATEGORIES)
+
+    def ecomm_prods(self):
+        return self.random_element(PRODUCTS)
+
 
 fk = Faker()
-
-# fk.pydecimal(l)
-# fk.word(ext_word_list=CATEGORIES)
-# fk.words(nb=3,ext_word_list=CATEGORIES,unique=True)
+fk.add_provider(Provider)
 
 
 class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Category
 
-    name = factory.Sequence(lambda n: 'categ_name_{}'.format(n))
+    # name = factory.Sequence(lambda n: 'categ_name_{}'.format(n))
+    name = fk.ecomm_categs()
 
 
 class ProductFactory(factory.django.DjangoModelFactory):
@@ -25,7 +37,7 @@ class ProductFactory(factory.django.DjangoModelFactory):
     description = FuzzyText(length=10)
     # description = ' '.join(fk.words(nb=5))
     categ = factory.SubFactory(CategoryFactory)
-    price = fk.pydecimal(left_digits=4,right_digits=2,min_value=0.00,max_value=1000)
+    price = fk.pydecimal(left_digits=4, right_digits=2, min_value=0.00, max_value=1000)
     sale = FuzzyChoice(choices=[False, False, False, True])
 
     # @factory.post_generation
@@ -34,4 +46,3 @@ class ProductFactory(factory.django.DjangoModelFactory):
     #         return
     #     print('do smth tomorrow')
     #     obj.title + some rand
-
